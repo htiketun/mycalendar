fd4c3f22997d0b2d9fd96cea752c99de04532836
htiketun <kohtiketun@outlook.com>
2025-03-10 06:52:10 +0000

    Improve markdown formatting; Update app title; Add new package; Improve utils documentation

--- lib/main.dart
+++ lib/main.dart
@@ -728,3 +728,4 @@ class CalendarApp extends StatelessWidget {
 // Commit 121: 2025-03-09T09:56:41
 // Commit 122: 2025-03-09T16:59:09
 // Commit 123: 2025-03-09T23:46:16
+// Commit 124: 2025-03-10T06:52:10
