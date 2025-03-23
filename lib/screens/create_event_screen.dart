d9db9d79731bf26fcdcadead8eb67beee4025526
htiketun <kohtiketun@outlook.com>
2025-03-23 20:36:57 +0000

    Improve event scheduling; Update description field; Add settings screen; Improve model relationships

--- lib/screens/create_event_screen.dart
+++ lib/screens/create_event_screen.dart
@@ -881,3 +881,4 @@ class _CreateEventScreenState extends State<CreateEventScreen>
 // Commit 147: 2025-03-17T01:59:48
 // Commit 164: 2025-03-22T02:52:27
 // Commit 169: 2025-03-23T13:51:21
+// Commit 170: 2025-03-23T20:36:57
